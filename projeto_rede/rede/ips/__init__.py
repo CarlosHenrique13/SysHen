e36a1af1c083eeceae797131f187b79f5e9db6bc
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-07-21 16:01:23 -0300

    edit interface

--- projeto_rede/rede/ips/__init__.py
+++ projeto_rede/rede/ips/__init__.py
@@ -3,4 +3,8 @@ from socket import *
 
 def descobre_ip(x,y):
     x = gethostbyname(y)
+    resutado =f"IP ({y}) = [{x}]"
+    if len(resutado) < 35:
+        for c in range(0,35-len(resutado)):
+            resutado += ' '
     return f"IP ({y}) = [{x}] "
